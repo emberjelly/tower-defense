7fa123365db2be93714542faa8095a2d2cda12d9
Jerry <bell.jezz@gmail.com>
2013-07-18 11:52:38 +1000

    Testing an update
    Added more comments to Main.h to indicate what needs to be done.

--- glfw-master/glfw_1/inc/Main.h
+++ glfw-master/glfw_1/inc/Main.h
@@ -35,7 +35,6 @@ void cursorPosCallback(GLFWwindow* window ,double x , double y );
 
 /* ****THINGS TO DO ****
 
-
 	* Figure out why towers sometimes shoot at enemies even though they are out of range. I think this has something to do with the way the member of my dynamic arrays are shifted around.
 	Meaning that if a tower has a target, and that target is destroyed, it is possible that that position in the array is given to another target, hence the tower would shoot at it regardless of distance.
 	
@@ -53,8 +52,14 @@ void cursorPosCallback(GLFWwindow* window ,double x , double y );
 	* Figure out how to add text to the menu
 
 	* Try to figure out a way to not have to use global variables for the input
-	
+
+	* Add a player header of some kind to store things like resources, number of enemies that have made it to the end of the path (or health of something).
+
 	* Add more tower and enemy types
-	
+
+	* Work on giving some structure to the rate, and type that enemies arrive arrive at to make the game playable.
+
+	* Add some more interface graphics to convey how much health the enemies have, maybe what enemies are coming next, available resources, health etc... 
+
 	* Redo graphics (hopefully in 3D eventually)
 */
